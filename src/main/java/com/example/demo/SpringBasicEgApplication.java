46198a2f2d910616bcd4621fbc3e81895b784baa
saylisonar <saylirsonar@gmail.com>
2021-12-23 18:00:42 +0530

    upload project

---
+++ src/main/java/com/example/demo/SpringBasicEgApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicEgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicEgApplication.class, args);
+	}
+
+}
